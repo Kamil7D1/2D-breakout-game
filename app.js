87b8e6a5dc0fc26e2d57ea5f3b1b2a7d1494befd
Kamil <kamilcz2001@gmail.com>
2023-12-18 15:34:09 +0100

    Add function which drawing ball.

---
+++ app.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+
+let ballRadius = 10;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
