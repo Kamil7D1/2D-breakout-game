0570f84839318f11f3e411c0fe9e022f4eb4d6ea
Kamil <kamilcz2001@gmail.com>
2023-12-18 15:40:50 +0100

    Add draw function which clear previously states on canvas.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
+let dx = 2;
+let dy = 2;
+
 let ballRadius = 10;
 
 function drawBall() {
@@ -13,3 +16,12 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // startX, startY, endX, endY. The whole area covered by this rectangle will be clered of any content painted there.
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
