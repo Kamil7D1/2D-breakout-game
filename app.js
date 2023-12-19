7225826f4ee2e0c25337baedd550b96ad1c5a294
Kamil <kamilcz2001@gmail.com>
2023-12-19 21:55:12 +0100

    Add a function that drawing bricks.

--- app.js
+++ app.js
@@ -4,54 +4,56 @@ const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
-let dx = 1;
-let dy = 1;
+let dx = 2;
+let dy = 2;
 
 let ballRadius = 10;
 
 function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
 }
 
-function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // startX, startY, endX, endY. The whole area covered by this rectangle will be clered of any content painted there.
-    drawBall();
-    x += dx;
-    y += dy;
-    // bottom / top edge
-    if (y + dy < ballRadius) {
-        dy = -dy;
-      } else if (y + dy > canvas.height - ballRadius) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
-          dy = -dy;
-        } else {
-          alert("GAME OVER");
-          document.location.reload();
-          clearInterval(interval);
-        }
-      }
-    // right / left edge
-    if (x + dx > canvas.width - ballRadius || x + dx <  ballRadius) {
-        dx = -dx;
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // startX, startY, endX, endY. The whole area covered by this rectangle will be clered of any content painted there.
+  drawBall();
+  x += dx;
+  y += dy;
+  // top edge
+  if (y + dy < ballRadius) {
+    dy = -dy;
+  } // bottom edge
+  else if (y > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      alert("GAME OVER");
+      document.location.reload();
+      clearInterval(interval);
     }
+  }
+  // right / left edge
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  // move right or left
+  if (rightPressed) {
+    paddleX += 3;
+  } else if (leftPressed) {
+    paddleX -= 3;
+  }
+  // condition check that is paddle is in boundaries
+  if (rightPressed) {
+    paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+  } else if (leftPressed) {
+    paddleX = Math.max(paddleX - 7, 0);
+  }
 
-    if (rightPressed) {
-        paddleX += 3;
-      } else if (leftPressed) {
-        paddleX -= 3;
-      }
-      
-    if (rightPressed) {
-        paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
-      } else if (leftPressed) {
-        paddleX = Math.max(paddleX - 7, 0);
-      }
-
-    drawPaddle()
+  drawPaddle();
+  drawBricks();
 }
 
 const interval = setInterval(draw, 10);
@@ -62,31 +64,63 @@ const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 
 function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
 }
 
 let rightPressed = false;
 let leftPressed = false;
 
 function keyDownHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight") {
-        rightPressed = true;
-    } else if (e.key === "Left" || e.key === "ArrowLeft") {
-        leftPressed = true;
-    }
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = true;
+  }
 }
 
 function keyUpHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight") {
-        rightPressed = false;
-    } else if (e.key === "Left" || e.key === "ArrowLeft") {
-        leftPressed = false;
-    }
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = false;
+  }
 }
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+const bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
+
+console.log(bricks);
+
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      bricks[c][r].x = 0;
+      bricks[c][r].y = 0;
+      ctx.beginPath();
+      ctx.rect(0, 0, brickWidth, brickHeight);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
